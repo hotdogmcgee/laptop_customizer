f2997d54aae19bc42ccd71b1c2d86775e8d5d619
Kevin Robinson <kevinrobinsondeveloper@gmail.com>
2019-05-14 16:59:42 -0400

    initial commit

---
+++ src/ListChoices/ListChoices.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+class ListChoices extends React.Component {
+
+}
+
+export default ListChoices
---
+++ src/SummaryItem/SummaryItem.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+class ShoppingItem extends React.Component {
+
+}
+
+export default ShoppingItem
